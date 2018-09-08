32a9b8990453e71d5a58509d10397636466c9164
Kyle Johnston <johnstonkylew@gmail.com>
2018-09-08 16:08:35 -0400

    Simplify code

--- make_plain_text_resume.py
+++ make_plain_text_resume.py
@@ -1,5 +1,5 @@
 # Created by Kyle Johnston on 2014-08-01
-# Last update: 2016-11-13
+# Last update: 2018-09-08
 
 # Get resume from user
 from tkinter.filedialog import askopenfilename
@@ -16,30 +16,19 @@ ActiveFileR = open(file_path, 'r') # LaTeX resume
 ActiveFileW = open(file_path[:len(file_path)-4] + '.txt', 'w') # plain text resume to save as
 
 DocumentStart = False # becomes true after reaching \begin{document}
-AddressStart = False # becomes true after starting address entry
-AddressEnd = False # becomes true after finishing address entry
 PrintLineCounter = 0 # indicates how many lines should be printed without consideration
 LineCountDown = 0 # indicates how many lines to wait before printing a line without consideration
 
-# Name
-p = re.compile(r'\\author\{([^}]*)}')
-for i, line in enumerate(ActiveFileR):
-    if p.search(line):
-        newline = p.sub(r'\1', line)
-        ActiveFileW.write(newline + '\n\n')
-        break
-
 for i, line in enumerate(ActiveFileR):
 
-    if DocumentStart == False and line.find('\\begin{document}') == -1:
+    if DocumentStart == False and line.find('%% PERSONAL INFO') == -1:
         pass
     else: # document has started, begin writing
-        DocumentStart = True # makes above if statement always false
+        DocumentStart = True # makes above 'if' statement always false
         PrintLine = False
-        AddressUpdated = False
         newline = line
         if PrintLineCounter != 0:
-            # print line without consideration; take 1 off of PrintLineCounter
+            # Print line without consideration; take 1 off of PrintLineCounter
             newline = RemoveBrackets(newline)
             PrintLine = True
             PrintLineCounter -= 1
@@ -56,34 +45,31 @@ for i, line in enumerate(ActiveFileR):
         p = re.compile(r'%.*')
         if p.search(newline):
             newline = p.sub(r'', newline)
-            if AddressStart == True and AddressEnd == False:
-                AddressUpdated = True # prevents accidental skipping of address
-                PrintLine = False # prevents printing a blank line
 
         # Address and contact information
-        if AddressEnd == False:
-            p = re.compile(r'\\bt\\')
-            if p.search(newline):
-                AddressStart = True
-                AddressUpdated = True
-                newline = newline.replace(' \\bt\\', '')
-                newline = RemoveBrackets(newline)
-                PrintLine = True
-                PrintLineCounter = 1
+        p = re.compile(r'\\newcommand{\\(.+?)}{(.+?)}')
+        if p.search(newline):
+            newline = p.sub(r'\2', newline)
+            PrintLine = True
 
-        # Check if done with address incase \bt\ used later in file
-        if AddressStart == True and AddressUpdated == False:
-            AddressEnd = True
+        # Objective
+        p = re.compile(r'\\section{OBJECTIVE}')
+        if p.search(newline):
+            newline = '\n\nOBJECTIVE\n\n'
+            PrintLine = True
+            LineCountDown = 5
 
-        # Sections and entries
-        headings = ['section', 'sect', 'sectlist']
-        for header in headings:
-            p = re.compile(r'[^\\]*\\' + header +'\{([^}]*)\}') # finds initial spaces, backslash, header, open bracket, text, close braket
-            if p.search(newline):
-                newline = p.sub(r'\n\n\1', newline) # print only text from above
-                PrintLine = True
-                if newline.find('OBJECTIVE') != -1:
-                    LineCountDown = 5
+        # sect
+        p = re.compile(r'[^\\]*\\' + 'sect' +'\{([^}]*)\}') # finds initial spaces, backslash, header, open bracket, text, close braket
+        if p.search(newline):
+            newline = p.sub(r'\n\n\1', newline) # print only text from above
+            PrintLine = True
+
+        # sectlist
+        p = re.compile(r'[^\\]*\\' + 'sectlist' +'\{([^}]*)\}') # finds initial spaces, backslash, header, open bracket, text, close braket
+        if p.search(newline):
+            newline = p.sub(r'\n\n\1\n', newline) # print only text from above
+            PrintLine = True
 
         # Entries
         entries = ['entry', 'school']
